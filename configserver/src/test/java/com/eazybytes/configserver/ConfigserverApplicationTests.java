407f7dd362f0040355944e9545b350ae53c69930
Ganesh Shet <ganeshgshet8@gmail.com>
2025-07-25 21:46:36 +0530

    mysql changes

--- configserver/src/test/java/com/eazybytes/configserver/ConfigserverApplicationTests.java
+++ configserver/src/test/java/com/eazybytes/configserver/ConfigserverApplicationTests.java
@@ -1,13 +1,13 @@
-package com.microservices.cards;
+package com.eazybytes.configserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CardsApplicationTests {
+class ConfigserverApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
