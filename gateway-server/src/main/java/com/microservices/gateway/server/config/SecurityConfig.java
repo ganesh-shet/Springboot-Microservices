c737a8896016fb21ee3d525acea1520a3c21c607
Ganesh Shet <ganesh.shet@volvo.com>
2025-11-28 19:55:16 +0530

    AuthCode grant type

--- gateway-server/src/main/java/com/microservices/gateway/server/config/SecurityConfig.java
+++ gateway-server/src/main/java/com/microservices/gateway/server/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.jwt.Jwt;
@@ -21,21 +20,21 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
         serverHttpSecurity.authorizeExchange(exchanges -> exchanges.pathMatchers(HttpMethod.GET).permitAll()
-                        .pathMatchers("/springbank/accounts/**").authenticated()
-                        .pathMatchers("/springbank/cards/**").authenticated()
-                        .pathMatchers("/springbank/loans/**").authenticated())
-                        .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec
-                        .jwt(Customizer.withDefaults()));
+                        .pathMatchers("/springbank/accounts/**").hasRole("ACCOUNTS")
+                        .pathMatchers("/springbank/cards/**").hasRole("CARDS")
+                        .pathMatchers("/springbank/loans/**").hasRole("LOANS"))
+                .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec
+                        .jwt(jwtSpec -> jwtSpec.jwtAuthenticationConverter(grantedAuthoritiesExtractor())));
         serverHttpSecurity.csrf(csrfSpec -> csrfSpec.disable());
         return serverHttpSecurity.build();
     }
 
-//    private Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthoritiesExtractor() {
-//        JwtAuthenticationConverter jwtAuthenticationConverter =
-//                new JwtAuthenticationConverter();
-//        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter
-//                (new KeycloakRoleConverter());
-//        return new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter);
-//    }
+    private Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthoritiesExtractor() {
+        JwtAuthenticationConverter jwtAuthenticationConverter =
+                new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter
+                (new KeycloakRoleConverter());
+        return new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter);
+    }
 
 }
